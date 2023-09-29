1874fb276e1dd11b8615d2cd5dd55c8dac56ea3e
bowtie <bowtie@users.noreply.github.com>
2023-09-30 00:14:50 +0100

    updates

---
+++ home/wezterm.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+let
+  weztem = "${pkgs.wezterm}/bin/wezterm start --cwd .";
+  wez = pkgs.writeShellScriptBin "wez" ''
+    if command -v "nixGLIntel" &> /dev/null; then
+        nixGLIntel ${weztem} "$@"
+    else
+        ${weztem} "$@"
+    fi
+  '';
+
+  substitute = name: pkgs.writeShellScriptBin name ''
+    wezterm "$@"
+  '';
+in 
+{
+  home = {
+    packages = [ wez (substitute "xterm") ];
+    sessionVariables.TERMINAL = "wez";
+  };
+
+  programs.wezterm.enable = true;
+  xdg.configFile.wezterm.source = ../config/wezterm;
+
+  xdg.desktopEntries."org.wezfurlong.wezterm" = {
+    name = "WezTerm";
+    comment = "Wez's Terminal Emulator";
+    icon = "org.wezfurlong.wezterm";
+    exec = "${wez}/bin/wez %F";
+    categories = [ "System" "TerminalEmulator" "Utility" ];
+    terminal = false;
+  };
+}
