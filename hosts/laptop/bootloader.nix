85ed91fb77b64cff68a073f3eff680947caef039
bowtie <bowtie@users.noreply.github.com>
2023-10-01 15:45:34 +0100

    add acer-wmi-battery pkg & kernel modules

--- hosts/laptop/bootloader.nix
+++ hosts/laptop/bootloader.nix
@@ -6,4 +6,14 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+
+  #build and load custom drivers
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    (callPackage ../../packages/acer-wmi-battery.nix {})
+  ];
+  boot.kernelModules = [ "acer-wmi-battery" "amd-pstate" ];
+  boot.kernelParams = [
+    "initcall_blacklist=acpi_cpufreq_init"
+    "amd_pstate.shared_mem=1"
+  ];
 }
