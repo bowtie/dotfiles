59ee237ae922444e3f362793ba7bc81ab69839da
bowtie <bowtie@users.noreply.github.com>
2023-10-25 12:58:42 +0100

    sync

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -18,5 +18,12 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
     };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 3 generations
+      options = "--delete-older-than +3";
+    };
   };
 }
