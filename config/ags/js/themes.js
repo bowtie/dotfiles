a090b6f876521597123e41bd8249a123339b49b5
bowtie <bowtie@users.noreply.github.com>
2023-11-12 14:15:42 +0000

    pls

--- config/ags/js/themes.js
+++ config/ags/js/themes.js
@@ -1,226 +1,84 @@
-import { Utils } from './imports.js';
-const WP = `/home/${Utils.USER}/Pictures/Wallpapers/`;
-
 /**
- * @param {Record<string, string>} scheme
- * @param {function(string): string} edit
- * @returns {Record<string, string>}
+ * A Theme is a set of options that will be applied
+ * ontop of the default values. see options.js for possible options
  */
-const editScheme = (scheme, edit) => {
-    /** @type {Record<string, string>} */
-    const obj = {};
-    Object.keys(scheme).forEach(color => {
-        obj[color] = edit(scheme[color]);
-    });
-    return obj;
-};
-
-const gnome = {
-    red: '#f66151',
-    green: '#57e389',
-    yellow: '#f6d32d',
-    blue: '#62a0ea',
-    magenta: '#c061cb',
-    teal: '#5bc8aF',
-    orange: '#ffa348',
-};
-
-const charm = {
-    red: '#e55f86',
-    green: '#00D787',
-    yellow: '#EBFF71',
-    blue: '#51a4e7',
-    magenta: '#9077e7',
-    teal: '#51e6e6',
-    orange: '#E79E64',
-};
-
-const tokyo = {
-    red: '#f7768e',
-    green: '#9ece6a',
-    yellow: '#e0af68',
-    blue: '#7aa2f7',
-    magenta: '#bb9af7',
-    teal: '#7dcfff',
-    orange: '#ff9e64',
-};
-
-const dark = {
-    color_scheme: 'dark',
-    bg_color: '#171717',
-    fg_color: '#eee',
-    hover_fg: '#f1f1f1',
-    ...charm,
-};
-
-const light = {
-    color_scheme: 'light',
-    bg_color: '#fffffa',
-    fg_color: '#141414',
-    hover_fg: '#0a0a0a',
-    ...editScheme(gnome, c => `darken(${c}, 8%)`),
-};
-
-const night = {
-    color_scheme: 'night',
-    bg_color: '#1a1b26',
-    fg_color: '#a9b1d6',
-    hover_fg: '#c0caf5',
-    ...tokyo,
-};
-
-const misc = {
-    wm_gaps: 22,
-    radii: 9,
-    spacing: 9,
-    shadow: 'rgba(0, 0, 0, .6)',
-    drop_shadow: true,
-    transition: 200,
-    screen_corners: true,
-    bar_style: 'normal',
-    layout: 'topbar',
-    desktop_clock: 'center center',
-    font: 'Ubuntu Nerd Font',
-    mono_font: 'Mononoki Nerd Font',
-    font_size: 16,
-};
-
-const colors = {
-    wallpaper_fg: 'white',
-    hypr_active_border: 'rgba(3f3f3fFF)',
-    hypr_inactive_border: 'rgba(3f3f3fDD)',
-    accent: '$blue',
-    accent_fg: '#141414',
-    widget_bg: '$fg_color',
-    widget_opacity: 94,
-    active_gradient: 'to right, $accent, lighten($accent, 6%)',
-    border_color: '$fg_color',
-    border_opacity: 97,
-    border_width: 1,
-};
-
-// themes
-const kitty_dark = {
-    wallpaper: WP + 'kitty.jpeg',
-    name: 'kitty_dark',
-    icon: '󰄛',
-    ...dark,
-    ...misc,
-    ...colors,
-};
-
-const kitty_light = {
-    wallpaper: WP + 'kitty.jpeg',
-    name: 'kitty_light',
-    icon: '󰄛',
-    ...light,
-    ...misc,
-    ...colors,
-    widget_bg: '$accent',
-    widget_opacity: 84,
-};
-
-const leaves_dark = {
-    ...kitty_dark,
-    wallpaper: WP + 'leaves.jpg',
-    name: 'leaves_dark',
-    icon: '󰌪',
-    accent: '$green',
-    widget_opacity: 92,
-    active_gradient: 'to right, $accent, darken($accent, 14%)',
-    border_opacity: 86,
-    bg_color: 'transparentize(#171717, 0.3)',
-    hypr_active_border: 'rgba(57e389FF)',
-    bar_style: 'floating',
-    radii: 0,
-    wallpaper_fg: 'white',
-    additional_scss: `
-        window#quicksettings .quicksettings {
-            margin-right: $wm_gaps;
-        }
-    `,
-};
-
-const leaves_light = {
-    ...leaves_dark,
-    ...light,
-    name: 'leaves_light',
-    accent: 'darken($green, 10%)',
-    bg_color: 'transparentize(#fff, 0.1)',
-    active_gradient: 'to right, $accent, darken($accent, 6%)',
-};
-
-const ivory = {
-    wallpaper: WP + 'ivory.png',
-    name: 'ivory',
-    icon: '󰟆',
-    ...light,
-    ...misc,
-    ...colors,
-    bg_color: '#fffff0',
-    bar_style: 'separated',
-    widget_bg: '$accent',
-    widget_opacity: 64,
-    drop_shadow: false,
-    border_width: 2,
-    border_opacity: 0,
-    active_gradient: 'to right, $accent, darken($accent, 6%)',
-    wallpaper_fg: '$bg_color',
-    hypr_active_border: 'rgba(111111FF)',
-    additional_scss: `
-        window#quicksettings .quicksettings,
-        window#dashboard .dashboard {
-            margin-top: 0;
-        }
-    `,
-};
-
-const cutefish = {
-    ...kitty_dark,
-    wallpaper: WP + 'cutefish.jpeg',
-    name: 'cutefish',
-    icon: '󰈺',
-    accent: 'lighten($blue, 20%)',
-    active_gradient: 'to right, $accent, lighten(mix($orange, $blue, 70%), 20%)',
-    border_opacity: 94,
-    brorder_width: 2,
-    radii: 9,
-    desktop_clock: 'end end',
-    hypr_active_border: 'rgba(3f3f3fFF)',
-    bg_color: 'transparentize(#171717, 0.3)',
-};
-
-const cherry = {
-    ...kitty_dark,
-    wallpaper: WP + 'cherry.png',
-    name: 'cherry',
-    icon: '',
-    accent: '$blue',
-    active_gradient: 'to right, $accent, lighten(mix($magenta, $blue, 70%), 18%)',
-    brorder_width: 2,
-    radii: 13,
-    bg_color: 'transparentize(#171717, 0.3)',
-};
-
-const tokyo_night = {
-    wallpaper: WP + 'market-garden.png',
-    name: 'tokyo_night',
-    icon: '🗼',
-    ...night,
-    ...misc,
-    ...colors,
-    accent: '$blue',
-    accent_fg: '#16161e',
-    active_gradient: 'to left, $accent, darken($accent, 6%)',
-};
+import { Theme, WP, lightColors } from './settings/theme.js';
 
 export default [
-    kitty_dark,
-    kitty_light,
-    leaves_dark,
-    leaves_light,
-    ivory,
-    cutefish,
-    cherry,
-    tokyo_night,
+    Theme({
+        name: 'Kitty Dark',
+        icon: '󰄛',
+    }),
+    Theme({
+        name: 'Kitty Light',
+        icon: '󰄛',
+        ...lightColors,
+        'widget.bg': '$accent',
+        'widget.opacity': 64,
+    }),
+    Theme({
+        name: 'Leaves',
+        icon: '󰌪',
+        'desktop.wallpaper': WP + 'leaves.jpg',
+        'accent.accent': '$green',
+        'widget.opacity': 92,
+        'accent.gradient': 'to right, $accent, darken($accent, 14%)',
+        'border.opacity': 86,
+        'color.bg': 'transparentize(#171717, 0.3)',
+        'hypr.active_border': 'rgba(57e389FF)',
+        'bar.style': 'floating',
+        'radii': 0,
+    }),
+    Theme({
+        name: 'Ivory',
+        icon: '󰟆',
+        ...lightColors,
+        'desktop.wallpaper': WP + 'ivory.png',
+        'desktop.fg_color': '$bg_color',
+        'desktop.screen_corners': false,
+        'bar.style': 'separated',
+        'widget.bg': '$accent',
+        'widget.opacity': 64,
+        'drop_shadow': false,
+        'border.width': 2,
+        'border.opacity': 0,
+        'accent.gradient': 'to right, $accent, darken($accent, 6%)',
+        'hypr.active_border': 'rgba(111111FF)',
+    }),
+    Theme({
+        name: 'Space',
+        icon: '',
+        'desktop.wallpaper': WP + 'space.jpg',
+        'spacing': 11,
+        'padding': 10,
+        'radii': 12,
+        'accent.accent': '$magenta',
+        'desktop.screen_corners': false,
+        'desktop.clock.enable': false,
+        'bar.separators': false,
+        'bar.icon': '',
+        'color.bg': 'transparentize(#171717, 0.3)',
+        'widget.opacity': 95,
+        'popover.padding.multiplier': 1.8,
+        'bar.flat_buttons': false,
+    }),
+    Theme({
+        name: 'Tokyo Night',
+        icon: '🗼',
+        'desktop.wallpaper': WP + 'market-garden.png',
+        'color.red': '#f7768e',
+        'color.green': '#9ece6a',
+        'color.yellow': '#e0af68',
+        'color.blue': '#7aa2f7',
+        'color.magenta': '#bb9af7',
+        'color.teal': '#7dcfff',
+        'color.orange': '#ff9e64',
+        'color.bg': '#1a1b26',
+        'color.fg': '#a9b1d6',
+        'hover_fg': '#c0caf5',
+        'accent.accent': '$blue',
+        'accent.gradient': 'to left, $accent, darken($accent, 6%)',
+        'desktop.screen_corners': false,
+        'bar.separators': false,
+    }),
 ];
