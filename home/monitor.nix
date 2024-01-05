a3edc61f457efac54e3fbece425c4669d3f7a748
bowtie <bowtie@users.noreply.github.com>
2024-01-05 15:12:25 +0000

    power

---
+++ home/monitor.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  script = pkgs.writeShellScript "power_monitor.sh" ''
+    BAT=$(echo /sys/class/power_supply/BAT*)
+    BAT_STATUS="$BAT/status"
+    BAT_CAP="$BAT/capacity"
+
+    AC_PROFILE="balanced"
+    BAT_PROFILE="power-saver"
+
+    # wait a while if needed
+    [[ -z $STARTUP_WAIT ]] || sleep "$STARTUP_WAIT"
+
+    # start the monitor loop
+    prev=0
+
+    while true; do
+    	# read the current state
+    	if [[ $(cat "$BAT_STATUS") == "Discharging" ]]; then
+      	profile=$BAT_PROFILE
+    	else
+    		profile=$AC_PROFILE
+    	fi
+
+    	# set the new profile
+    	if [[ $prev != "$profile" ]]; then
+    		echo setting power profile to $profile
+    		powerprofilesctl set $profile
+    	fi
+
+    	prev=$profile
+
+    	# wait for the next power change event
+    	inotifywait -qq "$BAT_STATUS" "$BAT_CAP"
+    done
+  '';
+
+  dependencies = with pkgs; [
+    coreutils
+    power-profiles-daemon
+    inotify-tools
+  ];
+in {
+  # Power state monitor. Switches Power profiles based on charging state.
+  # Plugged in - balanced (since ZEN 1 has no access to amd p_state)
+  # Unplugged - power-saver
+  systemd.user.services.power-monitor = {
+    Unit.Description = "Power Monitor";
+    Unit.After = ["power-profiles-daemon.service"];
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
+      Type = "simple";
+      ExecStart = script;
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+  };
+}
