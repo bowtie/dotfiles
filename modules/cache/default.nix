9ba6b9f24eaf5d49cf56950e22598a9310c7ac27
bowtie <bowtie@users.noreply.github.com>
2024-01-18 17:14:47 +0000

    update

--- modules/cache/default.nix
+++ modules/cache/default.nix
@@ -7,6 +7,7 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://ezkea.cachix.org"
     ];
 
     trusted-public-keys = [
@@ -15,6 +16,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
     ];
   };
 }
